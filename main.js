5fce6d76846a7407655197882e77d77a70bb3a85
Andika Nugraha <andika.nugraha@tokopedia.com>
2022-07-06 18:34:18 +0700

    fix

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const Octokit = require("@octokit/core").Octokit
 const githubToken = process.env.GITHUB_TOKEN
 const owner = process.env.OWNER
 const repo = process.env.REPO
+const service = core.getInput('service')
+const kdpHost = core.getInput('host')
+
 const octokit = new Octokit({ auth: githubToken });
 
 async function getLatestRelease(){
@@ -13,7 +16,28 @@ async function getLatestRelease(){
         repo: repo
     })
 
-    console.log(resp)
+    return release = {
+        pic : resp.data.author.login,
+        service: service,
+        tag: resp.data.html_url,
+        rfc: "-",
+        authors: extractAuthors(resp.data.body)
+    }
 }
 
-getLatestRelease()
+function extractAuthors(body) {
+    const authors = body.match(/@\w*/g)
+    return (authors || [])
+}
+
+function tellKDP(data){
+    console.log(data)
+}
+
+function main(){
+    const release = getLatestRelease()
+    tellKDP(release)
+    
+}
+
+main()
