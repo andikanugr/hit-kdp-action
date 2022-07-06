8f719e0de9c672fd8e48032e5e85eda8765363ee
Andika Nugraha <andika.nugraha@tokopedia.com>
2022-07-06 16:43:23 +0700

    test

--- hit_kdp.py
+++ hit_kdp.py
@@ -2,9 +2,19 @@ from github import Github
 import sys
 
 git = Github(sys.argv[1])
+org = sys.argv[2]
+repo = sys.argv[3]
+kdp_host = sys.argv[4]
+service = sys.argv[5]
+
+def get_latest_tag():
+    organization = git.get_organization(org)
+    repository = organization.get_repo(repo)
+    return repository.get_latest_release()
 
 def main():
     print("============ Starting =============")
-    print(sys.argv)
+    print(get_latest_tag())
 
 main()
+
