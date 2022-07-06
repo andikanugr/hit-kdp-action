c1399955b650dfc88733b7dcecf66933b5133288
Andika Nugraha <andika.nugraha@tokopedia.com>
2022-07-06 16:53:39 +0700

    test

--- hit_kdp.py
+++ hit_kdp.py
@@ -8,6 +8,8 @@ kdp_host = sys.argv[4]
 service = sys.argv[5]
 
 def get_latest_tag():
+    print(sys.argv[1])
+    print(git)
     organization = git.get_organization(org)
     repository = organization.get_repo(repo)
     return repository.get_latest_release()
